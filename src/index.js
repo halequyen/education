aa614696e02001e339aeb78995ae6cff613eaa91
unknown <halequyen19042001@gmail.com>
2022-03-27 14:18:33 +0700

    abc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const path = require('path');
 const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const handlebars  = require('express-handlebars');
-const app = express();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+
+db = low(adapter);
+
+db.defaults({ n1: [], n2: [], result: [] })
+  .write();
+
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,12 +31,15 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    result: db.get('result').value()
+  });
 });
 
-// app.post('/count', (req, res, next) => {
-//   res.render('home');
-// });
+app.post('/', (req, res) => {
+  db.get('result').push(req.body).write();
+  res.redirect('/');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
